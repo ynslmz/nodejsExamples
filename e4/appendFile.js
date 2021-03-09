bfed8556bdd2bc123ba54cc6b860f36917676901
Yunus ÖLMEZ <yns4832@hotmail.com>
2021-03-09 12:56:42 +0300

    fs events

---
+++ e4/appendFile.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var fs = require('fs');
+
+// debug and see differences
+
+http.createServer((req, res) => {
+    // The fs.appendFile() method appends specified content to a file. If the file does not exist, the file will be created!
+    // If you didn't locate your file's directory it'll create it in root of your app directory!
+
+    // this appends text to end of file
+    fs.appendFile('fsAppendtoJson.json', '{"hello":"world"}', (err) => {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+
+    // this method replaces the specified file and content 
+    fs.writeFile('fsAppendtoJson.json', '{"hello":"world"}', (err) => {
+        if (err) throw err;
+        console.log('saved!');
+    });
+    res.end('saved!');
+}).listen(8080);
